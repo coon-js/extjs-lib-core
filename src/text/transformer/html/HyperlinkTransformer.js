7c89d69b6f508464dd9b3359a27f4779d074d9be
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2020-02-23 09:31:27 +0100

    - enhancement: added HyperlinkTransformer (see coon-js/lib-cn_core#8)

---
+++ src/text/transformer/html/HyperlinkTransformer.js
@@ -0,0 +1,60 @@
+/**
+ * coon.js
+ * lib-cn_core
+ * Copyright (C) 2020 Thorsten Suckow-Homberg https://github.com/coon-js/lib-cn_core
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/**
+ * Transformer for transforming plain-quoted text containing Hyperlinks
+ * into text that wraps those Hyperlinks in "<a>"-tags.
+ *
+ * @example
+ *  let text = "This is an url https://www.conjoon.org and it is not clickable";
+ *
+ *  let transformer = Ext.create("coon.core.text.transformer.html.HyperlinkTransformer");
+ *
+ *  transformer.transform(text);
+ *
+ *  // returns:
+ *  // This is an url <a href="https://www.conjoon.org">https://www.conjoon.org</a> and it is not clickable
+ *
+ */
+Ext.define('coon.core.text.transformer.html.HyperlinkTransformer', {
+
+    /**
+     * Invokes transforming the passed string.
+     *
+     * @param {String} value
+     *
+     * @return {String}
+     */
+    transform : function(text) {
+
+        const urlRegex = /(\b(https?):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+
+        text = text.replace(urlRegex, matches => ("<a href=\"" + matches + "\">" + matches + "</a>"));
+
+        return text;
+
+    }
+
+});
